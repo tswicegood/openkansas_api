3a1082e1ade27efb1dc91d51c13fd4a954a620bb
Travis Swicegood <development@domain51.com>
2009-06-21 00:35:36 -0500

    modify this so its search/ instead of at the root

--- openkansas_api/urls.py
+++ openkansas_api/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls.defaults import patterns, url
 urlpatterns = patterns(
     'openkansas_api.views',
     url(r'^(representative|senator)/(\d+)/(.+)?$', 'handle_details', name="openkansas_api_details"),
-    url(r'^$', 'handle_query', name="openkansas_api_query"),
+    url(r'^search/$', 'handle_query', name="openkansas_api_query"),
 )
 
