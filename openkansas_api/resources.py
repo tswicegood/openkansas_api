9e210d4101f3e156589eb5bbfc6feb1bfdb97c1c
Travis Swicegood <development@domain51.com>
2009-06-25 00:09:30 -0500

    Add in APIs for getting information about representatives
    This works by adding ".json" or ".xml" to the end of any representatives
    page.  Still need to add an API for doing searches.
    
    Just want to take this opportunty to mention how great django-piston is.
    I literally set this API up from scratch in < 30 minutes.  Kudos to those
    guys.

---
+++ openkansas_api/resources.py
@@ -0,0 +1,39 @@
+from piston.handler import BaseHandler
+from openkansas_api.models import Representative
+
+REPRESENTATIVE_FIELDS = (
+    'district',
+    'type',
+    'full_name',
+    ('home_address', (
+        'street_address',
+        'city',
+        'state',
+        'zipcode',
+        'lat',
+        'lng',
+    )),
+    ('offices', (
+        'room',
+    )),
+    ('phone_numbers', (
+        'type',
+        'phone',
+    )),
+)
+
+
+class RepresentativeHandler(BaseHandler):
+    allowed_methods = ('GET',)
+    model = Representative
+    fields = REPRESENTATIVE_FIELDS
+    
+    def read(self, request, type, district):
+        real_type = type[0:3].upper()
+        representative = Representative.objects.get(type = real_type, district = district)
+        return representative
+
+
+class RepresentativeWithPolyHandler(RepresentativeHandler):
+    fields = REPRESENTATIVE_FIELDS + ('poly',)
+
--- openkansas_api/urls.py
+++ openkansas_api/urls.py
@@ -1,9 +1,29 @@
 from django.conf.urls.defaults import patterns, url
+from piston.resource import Resource
+from openkansas_api.resources import RepresentativeHandler, RepresentativeWithPolyHandler
 
 urlpatterns = patterns(
     'openkansas_api.views',
-    url(r'^(representative|senator)/(\d+)/(.+)?$', 'handle_details', name="openkansas_api_details"),
+    url(r'^(representative|senator)/(\d+)/([^.]+)?$', 'handle_details', name="openkansas_api_details"),
     url(r'^search/$', 'handle_query', name="openkansas_api_query"),
     url(r'^$', 'handle_index', name='openkansas_api_index'),
+
+)
+
+# various API related patterns
+representative_resource = Resource(handler=RepresentativeHandler)
+representative_with_poly_resource = Resource(handler=RepresentativeWithPolyHandler)
+
+urlpatterns += patterns('',
+    url(
+        r'^(?P<type>representative|senator)/(?P<district>\d+)/([^.]+)\.(?P<emitter_format>[a-z]+)$',
+        representative_resource,
+        name="openkansas_api_details.api"
+    ),
+    url(
+        r'^(?P<type>representative|senator)/(?P<district>\d+)/([^.]+)\.(?P<emitter_format>[a-z]+)/poly$',
+        representative_with_poly_resource,
+        name="openkansas_api_details.api.poly"
+    ),
 )
 
