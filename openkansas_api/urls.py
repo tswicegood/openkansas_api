9468aa303ce26f6728a0681f640280c51303b7c0
Travis Swicegood <development@domain51.com>
2009-06-25 21:28:16 -0500

    Move the API url definitions to happen first.
    I'm changing around the search URL structure to use JS and/or redirection
    to make the search URLs match the same URI structure as the rest of the
    site (i.e., add .xml or .json to get programatic info out of it).  This
    reorganization is just a precursor to that.

--- openkansas_api/urls.py
+++ openkansas_api/urls.py
@@ -2,20 +2,12 @@ from django.conf.urls.defaults import patterns, url
 from piston.resource import Resource
 from openkansas_api.resources import RepresentativeHandler, RepresentativeWithPolyHandler, SearchHandler
 
-urlpatterns = patterns(
-    'openkansas_api.views',
-    url(r'^(representative|senator)/(\d+)/([^.]+)?$', 'handle_details', name="openkansas_api_details"),
-    url(r'^search/$', 'handle_query', name="openkansas_api_query"),
-    url(r'^$', 'handle_index', name='openkansas_api_index'),
-
-)
-
 # various API related patterns
 representative_resource = Resource(handler=RepresentativeHandler)
 representative_with_poly_resource = Resource(handler=RepresentativeWithPolyHandler)
 search_resource = Resource(handler = SearchHandler)
 
-urlpatterns += patterns('',
+urlpatterns = patterns('',
     url(
         r'^(?P<type>representative|senator)/(?P<district>\d+)/([^.]+)\.(?P<emitter_format>[a-z]+)$',
         representative_resource,
@@ -31,5 +23,13 @@ urlpatterns += patterns('',
         search_resource,
         name='openkansas_api_query.api'
     ),
+)
+
+urlpatterns += patterns(
+    'openkansas_api.views',
+    url(r'^(representative|senator)/(\d+)/(.+)?$', 'handle_details', name="openkansas_api_details"),
+    url(r'^search/$', 'handle_query', name="openkansas_api_query"),
+    url(r'^$', 'handle_index', name='openkansas_api_index'),
+
 )
 
